5d1fb6fcbf98f8c30294c9af0d655b9c56cc75cb
shobhitexe <sguptamds94@gmail.com>
2024-04-15 09:19:20 +0530

    Mobile Navbar

--- src/components/Home/pricing/index.tsx
+++ src/components/Home/pricing/index.tsx
@@ -37,7 +37,7 @@ export default function Pricing() {
       </div>
 
       <div className="mt-20 flex sm:flex-row flex-col items-stretch justify-center md:gap-10 gap-5 sm:px-10 px-5">
-        <div className="bg-[#E2F3FC] p-5 md:w-[450px] w-fit basis-1/2 rounded-2xl flex flex-col justify-between items-stretch gap-5">
+        <div className="bg-[#E2F3FC] p-5 md:w-[450px] w-full max-md:basis-1/2 rounded-2xl flex flex-col justify-between items-stretch gap-5">
           <div className="bg-[#12729D] w-fit px-3 py-2 rounded-xl text-white font-circularBold md:text-2xl sm:text-xl xs:text-lg text-base">
             Landing Page
           </div>
@@ -74,7 +74,7 @@ export default function Pricing() {
           </div>
         </div>
 
-        <div className="bg-fullWebsiteCardBg p-5 md:w-[450px] w-full basis-1/2 rounded-2xl flex flex-col justify-between items-stretch gap-5">
+        <div className="bg-fullWebsiteCardBg p-5 md:w-[450px] w-full max-md:basis-1/2 rounded-2xl flex flex-col justify-between items-stretch gap-5">
           <div className="bg-white w-fit px-3 py-2 rounded-xl text-[#134B67] font-circularBold md:text-2xl sm:text-xl xs:text-lg text-base">
             Full Website
           </div>
--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -7,6 +7,8 @@ import { Link, animateScroll as scroll } from "react-scroll";
 export default function Navbar() {
   const [selected, setSelected] = useState<number>(0);
 
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
   const navBarLinks = [
     { title: "Home", href: "", to: "home" },
     { title: "Projects", href: "", to: "projects" },
@@ -18,37 +20,99 @@ export default function Navbar() {
   ];
 
   return (
-    <div className="bg-navBg text-white w-[80%] mx-auto mt-10 rounded-full fixed left-1/2 -translate-x-1/2 z-50">
-      <nav className="flex items-center justify-between p-3">
-        {navBarLinks.map((nav, idx) => {
-          if (idx === 3) {
-            return (
-              <Image
-                key={nav.title}
-                src={"/image/navbar/navbar.png"}
-                alt={"logo"}
-                width={200}
-                height={200}
-              />
-            );
-          }
+    <>
+      <div className="bg-navBg text-white lg:w-[80%] w-[90%] mx-auto mt-10 rounded-full fixed left-1/2 -translate-x-1/2 z-50">
+        <nav className="sm:flex hidden items-center justify-between md:p-3 sm:p-2 p-1 max-sm:px-3 max-sm:pb-3">
+          {navBarLinks.map((nav, idx) => {
+            if (idx === 3) {
+              return (
+                <Image
+                  key={nav.title}
+                  src={"/image/navbar/navbar.png"}
+                  alt={"logo"}
+                  width={200}
+                  height={200}
+                  className="md:w-[200px] w-[150px]"
+                />
+              );
+            }
 
-          return (
-            <Link
-              to={nav.to}
-              spy={true}
-              smooth={true}
-              key={nav.title}
-              onClick={() => setSelected(idx)}
-              className={`text-lg cursor-pointer py-5 px-10 font-circularBold ${
-                idx === selected && "bg-white text-black rounded-full"
-              }`}
+            return (
+              <Link
+                to={nav.to}
+                spy={true}
+                smooth={true}
+                key={nav.title}
+                onClick={() => setSelected(idx)}
+                className={`md:text-lg text-base cursor-pointer md:py-5 py-4 md:px-10 px-3 font-circularBold sm:flex hidden ${
+                  idx === selected && "bg-white text-black rounded-full"
+                }`}
+              >
+                {nav.title}
+              </Link>
+            );
+          })}
+        </nav>
+
+        <nav className="sm:hidden flex items-center justify-between md:p-3 sm:p-2 p-1 max-sm:px-3 max-sm:pb-3">
+          <Image
+            src={"/image/navbar/navbar.png"}
+            alt={"logo"}
+            width={150}
+            height={150}
+          />
+
+          {!isNavOpen ? (
+            <Image
+              src={"/image/navbar/ham.svg"}
+              alt={"logo"}
+              width={30}
+              height={30}
+              className="mr-4 mt-2 cursor-pointer"
+              onClick={() => setIsNavOpen(true)}
+            />
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={2}
+              stroke="currentColor"
+              className="w-8 h-8 mr-4 mt-2 cursor-pointer"
+              onClick={() => setIsNavOpen(false)}
             >
-              {nav.title}
-            </Link>
-          );
-        })}
-      </nav>
-    </div>
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 18 18 6M6 6l12 12"
+              />
+            </svg>
+          )}
+        </nav>
+      </div>
+
+      {isNavOpen && (
+        <div className="fixed w-full h-screen bg-navBg left-0 z-40">
+          <div className="flex flex-col justify-center items-center mt-36 gap-2">
+            {navBarLinks.map((nav, idx) => {
+              if (idx === 3) {
+                return;
+              }
+
+              return (
+                <Link
+                  to={nav.to}
+                  key={nav.title}
+                  className="text-white text-4xl font-circularBold cursor-pointer"
+                  onClick={() => setIsNavOpen(false)}
+                >
+                  {nav.title}
+                </Link>
+              );
+            })}
+          </div>
+        </div>
+      )}
+    </>
   );
 }
